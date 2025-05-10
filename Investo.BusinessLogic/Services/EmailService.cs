efc40174fe1421dbf27c9461b453b615e0ecc431
klasterr <kuzjovictor@gmail.com>
2025-05-10 20:12:46 +0300

    Refactore string.

--- Investo.BusinessLogic/Services/EmailService.cs
+++ Investo.BusinessLogic/Services/EmailService.cs
@@ -19,7 +19,7 @@ public class EmailService : IEmailService
         var message = new MailMessage();
         message.From = new MailAddress(this.emailSettings.From, "Investo");
         message.To.Add(new MailAddress(receiver));
-        message.Subject = "Reset Password Code - Inevsto";
+        message.Subject = "Reset Password Code - Investo";
         message.IsBodyHtml = true;
         message.Body = $"<p>Hello!</p>\r\n\r\n<p>You have requested a password reset for your <strong>Investo</strong> account.</p>\r\n\r\n<p>Your password reset code is:</p>\r\n\r\n<h2 style=\"color: #2d89ef;\">{code}</h2>\r\n\r\n<p>This code is valid for 10 minutes. If you did not make this request, please ignore this email.</p>\r\n\r\n<br />\r\n\r\n<p style=\"font-size: 0.9em; color: #777;\">Best regards,<br />\r\nThe Investo Team<br />\r\nnoreply.investo@gmail.com</p>\r\n";
 
